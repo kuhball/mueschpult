6b71a9e9dd3b006297b16ca8e917ceba51d26674
Simon Couball <s.couball@gmail.com>
2018-12-03 22:19:35 +0100

    added some stuff

--- readme.md
+++ readme.md
@@ -7,4 +7,46 @@ Dokumentation des Mischpults für die Jupibar im Gängeviertel.
 
 ## Benutze Libraries
 ### Tlc5940
-Benutzt für LEDs auf Panel.
+Benutzt für LEDs auf Frontpanel.
+
+## Hardware
+### Mux 4051
+8 Kanal analog Multiplexer für Input Potis.
+Pins für Mux:
+- A0
+- A1
+- A2
+
+## DME Setup List
+
+- Talkover für Mikrofone 50 / 51 (mic1, mic2) - binär 1/0
+- Mute für Mikrofone 52 / 53 (mic1, mic2) - 0/1
+- Fader Volume 54 / 55 (mic1, mic2) -  min -13801, max 1000
+- Meter 56 (alle Meter)
+  - 0 - alle Meter?
+  - 1 - Input DJ Oben
+  - 2 - Input Bar
+  - 3 - Spare Input
+  - 5 - Output Level Oben
+  - 6 - Output Level Bar
+  - 7 - Output Level Keller
+  - 15 - Mic 1 Level
+  - 16 - Mic 2 Level
+- Input Level Stereo in 1 - 58/59  min -13801, max 1000
+- Input Level Stereo in 2 - 60/61 min -13801, max 1000
+- Input Level Stereo in 3 - 62/63 min -13801, max 1000
+- Masterlevel Oben - 64/65
+- Masterlevel Bar - 66/67
+- Masterlevel Unten - 68/69
+- EQ Oben:
+  - High 70
+  - Mid 71
+  - Low 72
+- EQ Bar:
+  - High 73
+  - Mid 74
+  - Low 75
+- EQ Keller
+  - High 76
+  - Mid 77
+  - Low 78
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,134 @@
+/*INCLUDES*/
 #include <Arduino.h>
+#include "Tlc5940.h"
+#include <SoftwareSerial.h>
+#include <ArduinoSTL.h>
+#include <string.h>
+#include <map>
+
+
+/*DEBUG macros*/
+#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+#ifdef DEBUG    //Macros are usually in all capital letters.
+  #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
+  #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
+#else
+  #define DPRINT(...)     //now defines a blank line
+  #define DPRINTLN(...)   //now defines a blank line
+#endif
+
+/*DEFINES*/
+#define SERIAL_RX 2 //Serial Pins for DME Communication
+#define SERIAL_TX 3 //possible pins: 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI)
+#define SERIAL_BAUD 38400
+
+
+/*MUX STUFF*/
+
+// MUX1 fader 1-8
+#define PIN_MUX1 A0
+// Fader 9-16 and Button 1-6
+#define PIN_MUX2 A1
+#define PIN_MUX3 A2
+
+#define MUX_S0 6
+#define MUX_S1 7
+#define MUX_S2 8
+
+/*MUX STUFF*/
+
+
+enum Tlc5940Pins {
+	SCLK = 13, SIN=11, BLANK=10, XLAT= 9, GSCLK= 3,
+};
+
+
+struct inputpin {
+  int value;
+  int mux;
+  int pin;
+  int dme;
+};
+
+std::map<String, inputpin> inputs = {
+  {"test",{1,2,3,4}},
+  {"TALKOVER_MIC1",{-13801,0,PIN_MUX1,50}}
+};
+
+
+class FrontMux {
+public:
+  FrontMux(){
+  };
+private:
+  void setMux(int mux)
+	{
+		switch (mux) {
+			case 0:
+				digitalWrite(MUX_S0, LOW);
+				digitalWrite(MUX_S1, LOW);
+				digitalWrite(MUX_S2, LOW);
+				break;
+			case 1:
+				digitalWrite(MUX_S0, HIGH);
+				digitalWrite(MUX_S1, LOW);
+				digitalWrite(MUX_S2, LOW);
+				break;
+			case 2:
+				digitalWrite(MUX_S0, LOW);
+				digitalWrite(MUX_S1, HIGH);
+				digitalWrite(MUX_S2, LOW);
+				break;
+			case 3:
+				digitalWrite(MUX_S0, HIGH);
+				digitalWrite(MUX_S1, HIGH);
+				digitalWrite(MUX_S2, LOW);
+				break;
+			case 4:
+				digitalWrite(MUX_S0, LOW);
+				digitalWrite(MUX_S1, LOW);
+				digitalWrite(MUX_S2, HIGH);
+				break;
+			case 5:
+				digitalWrite(MUX_S0, HIGH);
+				digitalWrite(MUX_S1, LOW);
+				digitalWrite(MUX_S2, HIGH);
+				break;
+			case 6:
+				digitalWrite(MUX_S0, LOW);
+				digitalWrite(MUX_S1, HIGH);
+				digitalWrite(MUX_S2, HIGH);
+				break;
+			case 7:
+				digitalWrite(MUX_S0, HIGH);
+				digitalWrite(MUX_S1, HIGH);
+				digitalWrite(MUX_S2, HIGH);
+				break;
+			default:
+				break;
+		};
+};
+};
+
+class DME {
+public:
+  DME() {
+    Serial.println("Com started.");
+  };
+};
+
 
 void setup() {
-  // put your setup code here, to run once:
-}
+  Serial.begin(9600);
+
+  Tlc.init();
+  Tlc.clear();
+
+  if(inputs.find("test") != inputs.end()){
+    DPRINTLN(inputs["test"].dme);
+  };
+};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+
+};
